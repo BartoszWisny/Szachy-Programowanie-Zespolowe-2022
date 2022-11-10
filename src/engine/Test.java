4c0023fafb308ce8831ce6b1d31d86c1150618ce
Maurycy <96290280+Maurycy-898@users.noreply.github.com>
2022-11-10 08:37:41 +0100

    Test commit

--- src/engine/Test.java
+++ src/engine/Test.java
@@ -3,7 +3,7 @@ package engine;
 public class Test {
 
 	public static void main(String args[]) {
-		
+		// Test commit comment
 		//Board board = new Board("8/5k2/3p4/1p1Pp2p/pP2Pp1P/P4P1K/8/8 b - - 99 50");
 		//Board board = new Board("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2");
 		//Board board = new Board("4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1");
