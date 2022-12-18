b72808459ea9115615c41106f882c6c9c120a221
Wojtekm <123w456m@gmail.com>
2022-12-18 14:50:50 +0100

    Added login and register.

--- chess/src/components/LoginTiles.jsx
+++ chess/src/components/LoginTiles.jsx
@@ -3,9 +3,19 @@ import * as FaIcons from "react-icons/fa"
 import * as MdIcons from "react-icons/md"
 import styled from "styled-components"
 import Input from "./Input"
-import {signInWithPopup, FacebookAuthProvider, GoogleAuthProvider} from "firebase/auth"
-import {auth} from "../FirebaseConfig"
-import {Link} from "react-router-dom"
+import {
+  getAuth,
+  OAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  fetchSignInMethodsForEmail,
+  linkWithCredential,
+  getAdditionalUserInfo,
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  sendEmailVerification
+} from "firebase/auth"
+import { auth } from "../FirebaseConfig"
 
 const LoginTiles = () => {
   return (
@@ -22,24 +32,12 @@ const LoginTitle = styled.span`
   margin-left: 1rem;
 `
 
-/* function login() {
-  const auth = getAuth();
-  signInWithEmailAndPassword(auth, email, password)
-      .then((result) => {
-          
-      })
-      .catch(function (error) {
-          
-      });
-} */
-
 function loginGoogle() {
   const provider = new GoogleAuthProvider();
   
   signInWithPopup(auth, provider) // signInWithRedirect
     .then((result) => {
-      console.log(result); 
-      // Sth like: result.photoUrl.replace("s=96-c", "s=480-c") ...s=96-c --> s=480-c
+      console.log(result);
     }).catch((error) => {
       console.log(error); 
     });
@@ -50,6 +48,17 @@ function loginFacebook() {
   
   signInWithPopup(auth, provider) // signInWithRedirect
     .then((result) => {
+      const {isNewUser} = getAdditionalUserInfo(result)
+
+      if(isNewUser) {
+        const user = result.user;
+
+        sendEmailVerification(user).then((result) => {
+          console.log(result);
+        }).catch((error) => {
+          console.log(error);
+        });
+      }
       console.log(result); // GraphAPI access token: EAAPtF26rDYYBAO8JlwxKXjUvGuvENvY3lrIuip7mPPJnDCO7CETZCJ07zwrFXV5CZA7BdmYMJm9pNZCqxY1ddH4tIGcwQM6t9zc0ZCitdMfAP884wKUPV4PEHquoEJiiasNZBW6kUESJy54ZB0FLa4hdWSF07VNpwx6j62oTKokgBgJiOPkmHBEIRAZCYHFQeiyRzWvUJ9TzdZBnKaPK7ONoOUya3U67PhuU52Eq063SNGgAZCxHuzzkO
       // https://graph.facebook.com/1241216816608059/picture?width=480&access_token=EAAPtF26rDYYBAO8JlwxKXjUvGuvENvY3lrIuip7mPPJnDCO7CETZCJ07zwrFXV5CZA7BdmYMJm9pNZCqxY1ddH4tIGcwQM6t9zc0ZCitdMfAP884wKUPV4PEHquoEJiiasNZBW6kUESJy54ZB0FLa4hdWSF07VNpwx6j62oTKokgBgJiOPkmHBEIRAZCYHFQeiyRzWvUJ9TzdZBnKaPK7ONoOUya3U67PhuU52Eq063SNGgAZCxHuzzkO
       // Scheme: https://graph.facebook.com/USER_ID/picture?width=480&access_token=ACCESS_TOKEN
@@ -57,10 +66,47 @@ function loginFacebook() {
     })
     .catch((error) => {
       console.log(error);
+
+      if(error.code === 'auth/account-exists-with-different-credential') {
+        const credential = OAuthProvider.credentialFromError(error);
+        const email = error.customData.email;
+
+        fetchSignInMethodsForEmail(auth, email).then((result) => {
+          if (result[0] === 'password') {
+            const password = "asdffdsa"; // TODO popup do wpisania hasła
+
+            signInWithEmailAndPassword(auth, email, password).then((result) => {
+              linkWithCredential(auth.currentUser, credential).then((result) => {
+                console.log(result);
+              }).catch((error) => {
+                console.log(error);
+              });
+            }).catch((error) => {
+              console.log(error);
+            });
+
+            return;
+          }
+
+          if (result[0] === 'google.com') {
+            const provider = new GoogleAuthProvider();
+
+            signInWithPopup(auth, provider).then((result) => {
+              linkWithCredential(auth.currentUser, credential).then((result) => {
+                console.log(result);
+              }).catch((error) => {
+                console.log(error);
+              });
+            });
+          }
+        }).catch((error) => {
+          console.log(error);
+        });
+      }
     });
 }
 
-/* function signOut() {
+function signOut() {
   const auth = getAuth();
   signOut().then(() => {
       // Sign-out successful.
@@ -68,12 +114,23 @@ function loginFacebook() {
       console.log(error)
       // An error happened.
   });
-} */
-  
+}
+
 function LoginTile() {
+  function login() {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   /* const navigate = useNavigate();
-  const signInRoute = () => {
-    navigate("/signin");
+  const route = () => {
+    navigate(props.path);
   }; */
 
   const [email, setEmail] = useState("")
@@ -90,9 +147,9 @@ function LoginTile() {
 
   return(
     <div className="logintile">
-      <Input id="email" type="email" placeholder="Email" value={email} onChange={handleEmailChange}/>
+      <Input id="email" type="text" placeholder="Email" value={email} onChange={handleEmailChange}/>
       <Input id="password" type="password" placeholder="Password" value={password} onChange={handlePasswordChange}/>
-      <button className="logintile_button1" /* onClick={login} */>
+      <button className="logintile_button1"  onClick={login} >
         <LoginIcon>
           <MdIcons.MdEmail />
           <LoginTitle>
@@ -117,8 +174,6 @@ function LoginTile() {
           </LoginTitle>
         </LoginIcon>
       </button>
-      <b className="logintile_signuplink">Don't have an account yet? <Link to="/signup" 
-      style={{color: "var(--primary)"}}>Sign up</Link></b>
     </div>
   )
 }
