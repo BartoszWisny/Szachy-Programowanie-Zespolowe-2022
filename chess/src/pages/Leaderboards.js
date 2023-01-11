c6bf512559d6b9b889aebf2b4e19f7a00a3dbdb7
JakubS26 <jakub_sokolowski@onet.pl>
2023-01-11 21:55:47 +0100

    Getting leaderboards from database

--- chess/src/pages/Leaderboards.js
+++ chess/src/pages/Leaderboards.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState, useCallback} from "react"
 import "./Leaderboards.css"
 import {Helmet} from "react-helmet"
 import SidebarMenu from "../components/SidebarMenu"
@@ -6,6 +6,8 @@ import useLocalStorage from "use-local-storage"
 import styled from "styled-components"
 import * as IoIcons from "react-icons/io"
 import {GridLoader} from "react-spinners"
+import { collection, getDocs } from "firebase/firestore"
+import {database} from "../FirebaseConfig"
 
 const SwitchThemeButton = styled.button`
   background-color: var(--primary);
@@ -22,6 +24,7 @@ const SwitchThemeButton = styled.button`
   top: 0;
 `
 
+
 function Leaderboards() {
   const [theme, setTheme] = useLocalStorage("theme" ? "darkmode" : "lightmode")
   
@@ -32,12 +35,38 @@ function Leaderboards() {
 
   const [loading, setLoading] = useState(false)
 
+// const data = [
+//     { username: "Player1", wins: 21, draws: 7, defeats: 2, points: 290},
+//     { username: "Player2", wins: 19, draws: 8, defeats: 4, points: 265},
+//     { username: "Player3", wins: 18, draws: 9, defeats: 4, points: 254},
+//     { username: "Player4", wins: 17, draws: 9, defeats: 9, points: 203},
+//     { username: "Player5", wins: 6, draws: 11, defeats: 10, points: 102},
+//     { username: "Player6", wins: 6, draws: 3, defeats: 1, points: 92},
+//     { username: "Player7", wins: 3, draws: 0, defeats: 4, points: 70},
+//   ]
+
   useEffect(() => {
     setLoading(true)
     setTimeout(() => {
       setLoading(false)
     }, 2000)
   }, [])
+
+  const [leaderboards, setLeaderboards] = useState([]);
+
+  const fetchData = useCallback(async () => {
+    await getDocs(collection(database, "leaderboards"))
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs
+        .map((doc) => ({...doc.data(), id:doc.id}))
+        setLeaderboards(data)
+        // console.log(data)
+    })
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
 
   return (
     <div className="leaderboards" data-theme={theme}>
@@ -48,16 +77,42 @@ function Leaderboards() {
           <meta name="description" content="Title" />
       </Helmet>
       <SidebarMenu />
-      <div className="img" data-theme={theme}/>
       {loading ? 
         <div>
           <GridLoader color={theme === "lightmode" ? "var(--primary)" : "var(--secondary)"} loading={loading} size={50} 
           speedMultiplier={1} style={{position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)",
           userSelect: "none"}}/>
-        </div> : null }
+        </div> : 
+          <div className="score_table">
+          <table className="score_table_columns">
+            <thead className="score_table_header">
+              <tr>
+                <th>Username</th>
+                <th>Wins</th>
+                <th>Draws</th>
+                <th>Defeats</th>
+                <th>Points</th>
+              </tr>
+            </thead>
+            {leaderboards.sort(function(a, b){return b.points - a.points}).map((val, key) => {
+             return (
+              <tbody key={key}>
+                <tr>
+                  <td><b>{val.username}</b></td>
+                  <td>{val.gamesWon}</td>
+                  <td>{val.draws}</td>
+                  <td>{val.gamesLost}</td>
+                  <td>{val.points}</td>
+                </tr>
+              </tbody>
+            )
+            })}
+         </table>
+        </div> }
       <SwitchThemeButton onClick={switchTheme}>
         {theme === "lightmode" ? (<IoIcons.IoIosSunny />) : (<IoIcons.IoIosMoon />)}
       </SwitchThemeButton>
+      {console.log(leaderboards)}
     </div>
   )
 }
