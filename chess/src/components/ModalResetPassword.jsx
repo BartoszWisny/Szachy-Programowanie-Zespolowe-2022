e52055c687f7123cc9cb8551628e54a14cf06735
Wojtekm <123w456m@gmail.com>
2023-01-13 00:56:01 +0100

    Added reset password option

---
+++ chess/src/components/ModalResetPassword.jsx
@@ -0,0 +1,62 @@
+import React, {useState} from "react"
+import "./ModalResetPassword.css"
+import {Link} from "react-router-dom"
+import ModalInput from "./ModalInput";
+import {getAuth, sendPasswordResetEmail} from "firebase/auth";
+import {NotificationManager} from "react-notifications";
+import * as IoIcons from "react-icons/io";
+import styled from "styled-components";
+
+const NavIcon = styled(Link)`
+  color: var(--primary);
+  opacity: 0.98;
+  margin-top: 0.4rem;
+  margin-right: 0.6rem;
+  margin-left: auto;
+  font-size: 1.4rem;
+  height: 2.2rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`
+
+const ModalResetPassword = ({open, stateChanger}) => {
+  const [email, setEmail] = useState("")
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value)
+  }
+
+  function sendResetEmail() {
+    const auth = getAuth();
+
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+      })
+      .catch((error) => {
+      });
+
+    NotificationManager.info('A reset email was sent if a matching account was found.', '', 5000, () => {});
+  }
+
+  const handleClose = event => {
+    stateChanger(false);
+  }
+
+  return (open ?
+    <div className="overlayresetpassword">
+      <div className="modalresetpassword_container">
+        <NavIcon to="#" draggable="false">
+          <IoIcons.IoIosCloseCircle onClick={handleClose}/>
+        </NavIcon>
+        <p className="modalresetpassword_title" style={{fontSize: "min(1rem, 3.2vw)"}}>
+        Provide the email address linked to your account:</p>
+        <ModalInput id="email" placeholder="Email" value={email} onChange={handleEmailChange}/>
+        <button className="modalresetpassword_button1" onClick={sendResetEmail}>Send password change email.</button>
+      </div>
+    </div>
+    : null
+  )
+}
+
+export default ModalResetPassword
