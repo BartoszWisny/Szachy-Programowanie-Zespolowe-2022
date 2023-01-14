a1e1f9859e4c19a48757e5ae4f6b2865382158ae
BartoszWisny <261708@student.pwr.edu.pl>
2023-01-14 02:01:03 +0100

    Repaired font bugs

--- chess/src/pages/About.js
+++ chess/src/pages/About.js
@@ -54,7 +54,41 @@ function About() {
           <GridLoader color={theme === "lightmode" ? "var(--primary)" : "var(--secondary)"} loading={loading} size={50} 
           speedMultiplier={1} style={{position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)",
           userSelect: "none"}}/>
-        </div> : null}
+        </div> :
+        <div> 
+          <div className="title" style={{color: theme === "lightmode" ? "var(--primary)" : "var(--secondary)"}}>
+            <p>Welcome to <b> Chess - Learn And Play </b></p>
+          </div>
+          <div className="description" style={{color: theme === "lightmode" ? "var(--primary)" : "var(--secondary)"}}>
+            
+            We are happy that you decided to practice your chess skills with us. On our website you can play chess <br />
+            in four different ways. You can play online with other users with the <b>Play 1 vs 1 (online)</b> mode. <br />
+            You can also play with your friends on the same device using the <b>Play 1 vs 1 (offline)</b> mode. <br />
+            If you'd like to have a go at playing against a professional chess engine (Stockfish), choose <b>Play vs computer</b>. <br />
+            In the section <b>Play vs our chess AI</b> you can play against an experimental engine that we created from scratch.<br /> <br />
+
+            If you feel like learning something new, visit <b>Watch games</b> and <b>Analyze Games</b> sections. There you can <br />
+            watch other players' games and try to draw conclusions, either on your own or with the help of a chess engine. <br /> <br />
+
+            If you like more extraordinary challenges that require strategic thinking, you can open the <b>Puzzles</b> section. <br />
+            Every 15 seconds a new riddle is fetched from a database. Will you be able to checkmate your opponent in one move only? <br /> <br />
+
+            You can also find out how good you are at chess and compare yourself to other users by checking <br />
+            your position in our <b>Leaderboards</ b>. <br /> <br />
+
+            Do you want to give us some feedback or report a bug? Go to the <b>Feedback</b> tab and share your opinion with us. <br/>
+            We will be grateful for any ideas that will help us improve this website. <br /> <br />
+
+          </div>
+
+          <div className="footer" style={{color: theme === "lightmode" ? "var(--primary)" : "var(--secondary)"}}>
+            <p><b>Created by:</b></p> 
+            <p> Bartosz Wiśny, Jakub Sokołowski, Wojciech Mulka, Maurycy Sosnowski, Karol Dzwonkowski </p> 
+            <p> You can contact our team at: chesslearnandplay@gmail.com </p>
+          </div>
+
+        </div>
+        }
       <SwitchThemeButton onClick={switchTheme} style={{zIndex: "9"}}>
         {theme === "lightmode" ? (<IoIcons.IoIosSunny />) : (<IoIcons.IoIosMoon />)}
       </SwitchThemeButton>
