8ea8dc6b2a6572e19933b8a90483ff916f73eb49
BartoszWisny <261708@student.pwr.edu.pl>
2023-01-15 15:04:46 +0100

    Adjusted about page for mobile devices

--- chess/src/pages/About.js
+++ chess/src/pages/About.js
@@ -55,28 +55,28 @@ function About() {
           speedMultiplier={1} style={{position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)",
           userSelect: "none"}}/>
         </div> :
-        <div> 
+        <div style={{maxWidth: "min(60rem, 90vw)", margin: "min(4.8rem, min(5vw, 5vh)) auto"}}> 
           <div className="title" style={{color: theme === "lightmode" ? "var(--primary)" : "var(--secondary)"}}>
             <p>Welcome to <b> Chess - Learn And Play </b></p>
           </div>
           <div className="description" style={{color: theme === "lightmode" ? "var(--primary)" : "var(--secondary)"}}>
             
-            We are happy that you decided to practice your chess skills with us. On our website you can play chess <br />
-            in four different ways. You can play online with other users with the <b>Play 1 vs 1 (online)</b> mode. <br />
-            You can also play with your friends on the same device using the <b>Play 1 vs 1 (offline)</b> mode. <br />
-            If you'd like to have a go at playing against a professional chess engine (Stockfish), choose <b>Play vs computer</b>. <br />
+            We are happy that you decided to practice your chess skills with us. On our website you can play chess
+            in four different ways. You can play online with other users with the <b>Play 1 vs 1 (online)</b> mode.
+            You can also play with your friends on the same device using the <b>Play 1 vs 1 (offline)</b> mode.
+            If you'd like to have a go at playing against a professional chess engine (Stockfish), choose <b>Play vs computer</b>.
             In the section <b>Play vs our chess AI</b> you can play against an experimental engine that we created from scratch.<br /> <br />
 
-            If you feel like learning something new, visit <b>Watch games</b> and <b>Analyze Games</b> sections. There you can <br />
+            If you feel like learning something new, visit <b>Watch games</b> and <b>Analyze Games</b> sections. There you can
             watch other players' games and try to draw conclusions, either on your own or with the help of a chess engine. <br /> <br />
 
-            If you like more extraordinary challenges that require strategic thinking, you can open the <b>Puzzles</b> section. <br />
+            If you like more extraordinary challenges that require strategic thinking, you can open the <b>Puzzles</b> section.
             Every 15 seconds a new riddle is fetched from a database. Will you be able to checkmate your opponent in one move only? <br /> <br />
 
-            You can also find out how good you are at chess and compare yourself to other users by checking <br />
+            You can also find out how good you are at chess and compare yourself to other users by checking
             your position in our <b>Leaderboards</ b>. <br /> <br />
 
-            Do you want to give us some feedback or report a bug? Go to the <b>Feedback</b> tab and share your opinion with us. <br/>
+            Do you want to give us some feedback or report a bug? Go to the <b>Feedback</b> tab and share your opinion with us.
             We will be grateful for any ideas that will help us improve this website. <br /> <br />
 
           </div>
