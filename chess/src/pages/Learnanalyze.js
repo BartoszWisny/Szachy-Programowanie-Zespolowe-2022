a1372b5c187ebefd1b0257bfa5e0227c5e2923bd
BartoszWisny <261708@student.pwr.edu.pl>
2023-01-18 00:29:20 +0100

    Started making watch and analyze games

--- chess/src/pages/Learnanalyze.js
+++ chess/src/pages/Learnanalyze.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState, useCallback} from "react"
 import "./Learnanalyze.css"
 import {Helmet} from "react-helmet"
 import SidebarMenu from "../components/SidebarMenu"
@@ -6,6 +6,9 @@ import useLocalStorage from "use-local-storage"
 import styled from "styled-components"
 import * as IoIcons from "react-icons/io"
 import {GridLoader} from "react-spinners"
+import { onAuthStateChanged, getAuth } from "firebase/auth"
+import { collection, getDocs, query, where } from "firebase/firestore"
+import {database} from "../FirebaseConfig"
 
 const SwitchThemeButton = styled.button`
   background-color: var(--primary);
@@ -39,6 +42,104 @@ function Learnanalyze() {
     }, 2000)
   }, [])
 
+  const [user, setUser] = useState(null)
+  const auth = getAuth()
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(auth.currentUser)
+      }
+    })
+  }, [auth])
+
+  const [games, setGames] = useState([])
+
+  const fetchData = useCallback(async () => {
+    let conditions = []
+    conditions.push(where("whiteID", "==", user.uid))
+    conditions.push(where("status", "==", "ended"))
+    const query1 = query(collection(database, "games"), ...conditions)
+
+    await getDocs(query1)
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs
+        .map((doc) => ({...doc.data(), id:doc.id}))
+        setGames(array => array.concat(data))
+    }) 
+
+    conditions = []
+    conditions.push(where("blackID", "==", user.uid))
+    conditions.push(where("status", "==", "ended"))
+    const query2 = query(collection(database, "games"), where("blackID", "==", user.uid), where("status", "==", "ended"))
+    await getDocs(query2)
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs
+        .map((doc) => ({...doc.data(), id:doc.id}))
+        setGames(array => array.concat(data))
+    })
+  }, [user])
+
+  const [fetched, setFetched] = useState(false)
+  const [userPieces, setUserPieces] = useState([])
+
+  useEffect(() => {
+    if(user !== null && games.length === 0 && !fetched) {
+      fetchData()
+      setFetched(true)
+    }
+
+    const getUserGamesData = async () => {
+      if (user !== null && games !== null) {
+        for (let i = 0; i < games.length; i++) {
+          if (games[i].whiteID === user.uid) {
+            setUserPieces(array => array.concat(["w"]))
+
+            if (games[i].result === "white") {
+              games[i].result = "Win"
+            } else if (games[i].result === "black") {
+              games[i].result = "Lose"
+            } else if (games[i].result === "draw") {
+              games[i].result = "Draw"
+            }
+          } else if (games[i].blackID === user.uid) {
+            setUserPieces(array => array.concat(["b"]))
+
+            if (games[i].result === "white") {
+              games[i].result = "Lose"
+            } else if (games[i].result === "black") {
+              games[i].result = "Win"
+            } else if (games[i].result === "draw") {
+              games[i].result = "Draw"
+            }
+          }
+        }
+
+        for (let i = 0; i < games.length; i++) {
+          let whitePlayerID = games[i].whiteID
+          let blackPlayerID = games[i].blackID  
+          const userCollectionRef = collection(database, "leaderboards")
+          const getWhiteUser = query(userCollectionRef, where("userID", "==", whitePlayerID))
+          const querySnapshotWhite = await getDocs(getWhiteUser)
+    
+          querySnapshotWhite.forEach((doc) => {
+            games[i].whiteID = doc.data().username
+          })
+    
+          const getBlackUser = query(userCollectionRef, where("userID", "==", blackPlayerID))
+          const querySnapshotBlack = await getDocs(getBlackUser)
+    
+          querySnapshotBlack.forEach((doc) => {
+            games[i].blackID = doc.data().username
+          })
+        }
+      }
+    }      
+
+    getUserGamesData()
+  }, [games, fetchData, fetched, user])
+
+
   return (
     <div className="learnanalyze" data-theme={theme}>
       <Helmet>
@@ -54,7 +155,33 @@ function Learnanalyze() {
           <GridLoader color={theme === "lightmode" ? "var(--primary)" : "var(--secondary)"} loading={loading} size={50} 
           speedMultiplier={1} style={{position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)",
           userSelect: "none"}}/>
-        </div> : null }
+        </div> : 
+        <div className="games_table" style={{zIndex: 0}}>
+          <table className="games_table_columns">
+            <thead className="games_table_header">
+              <tr>
+                <th>User</th>
+                <th></th>
+                <th>Opponent</th>
+                <th>Result</th>
+                <th>Analyze</th>
+              </tr>
+            </thead>
+            {games !== null && games.map((val, key) => {
+              return (
+                <tbody key={key}>
+                  <tr>
+                    <td><b>{userPieces[key] === "w" ? val.whiteID : val.blackID}</b></td>
+                    <td>vs</td>
+                    <td><b>{userPieces[key] === "w" ? val.blackID : val.whiteID}</b></td>
+                    <td><b>{val.result}</b></td>
+                    <td><button className="analyze_button">Analyze</button></td>
+                  </tr>
+                </tbody>
+              )
+            })}
+          </table>
+        </div>}
       <SwitchThemeButton onClick={switchTheme} style={{zIndex: "9"}}>
         {theme === "lightmode" ? (<IoIcons.IoIosSunny />) : (<IoIcons.IoIosMoon />)}
       </SwitchThemeButton>
--- chess/src/pages/Learnwatch.js
+++ chess/src/pages/Learnwatch.js
@@ -39,12 +39,130 @@ function Learnwatch() {
     }, 2000)
   }, [])
 
-  /* const getGMs = useCallback(async () => {
-    const url = "https://api.chess.com/pub/titled/GM"
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data)  
-  }, []) */
+  /* function shuffle() { 
+    return 0.5 - Math.random() 
+  } 
+
+  function getRandomInt(int) {
+    return Math.floor(Math.random() * int)
+  }
+
+  // const [games, setGames] = useState([])
+  const [game, setGame] = useState(null) */
+
+  /* const getGMGames = useCallback(async () => {
+    const url1 = "https://api.chess.com/pub/titled/GM"
+    const response1 = await fetch(url1)
+    const data1 = await response1.json()
+    const GMs = data1.players
+    const randomGM = GMs[getRandomInt(GMs.length)]
+    const url2 = `https://api.chess.com/pub/player/${randomGM}`
+    const response2 = await fetch(url2)
+    const data2 = await response2.json()
+    const gmname = data2.name ? data2.name : data2.username
+    const url3 = `https://api.chess.com/pub/player/${randomGM}/games/archives`
+    const response3 = await fetch(url3)
+    const data3 = await response3.json()
+    const gmarchives = data3.archives
+    const randomarchive = getRandomInt(gmarchives.length)
+    const archive = gmarchives[randomarchive]
+    const url4 = archive
+    const response4 = await fetch(url4)
+    const data4 = await response4.json()
+    const gmgames = data4.games
+    const randomgame = getRandomInt(gmgames.length)
+    const gmgame = gmgames[randomgame]
+    const gmgametype = gmgame.rules
+    let gmgameresult = ""
+    let gmopponentname = "" */
+
+
+    // gms.sort(shuffle)
+    // const randomgms = gms.slice(0, 5)
+    // let games = []
+
+    /* for (let i = 0; i < randomgms.length; i++) {
+      const url2 = `https://api.chess.com/pub/player/${randomgms[i]}`
+      const response2 = await fetch(url2)
+      const data2 = await response2.json()
+      const gmname = data2.name ? data2.name : data2.username
+      const url3 = `https://api.chess.com/pub/player/${randomgms[i]}/games/archives`
+      const response3 = await fetch(url3)
+      const data3 = await response3.json()
+      const gmarchives = data3.archives
+      const randomarchive = getRandomInt(gmarchives.length)
+      const archive = gmarchives[randomarchive]
+      const url4 = archive
+      const response4 = await fetch(url4)
+      const data4 = await response4.json()
+      const gmgames = data4.games
+      const randomgame = getRandomInt(gmgames.length)
+      const gmgame = gmgames[randomgame]
+      const gmgametype = gmgame.rules
+      let gmgameresult = ""
+      let gmopponentname = ""
+    
+      if (gmgame.black["@id"] === `https://api.chess.com/pub/player/${randomgms[i]}`) {
+        gmgameresult = gmgame.black.result === "win" ? "Win" : 
+                       (gmgame.black.result === "chechmated" ? "Checkmated" :
+                       (gmgame.black.result === "agreed" ? "Draw agreed" : 
+                       (gmgame.black.result === "repetition" ? "Draw by repetition" : 
+                       (gmgame.black.result === "timeout" ? "Timeout" : 
+                       (gmgame.black.result === "resigned" ? "Resigned" : 
+                       (gmgame.black.result === "stalemate" ? "Stalemate" : 
+                       (gmgame.black.result === "lose" ? "Lose" : 
+                       (gmgame.black.result === "insufficient" ? "Insufficient material" : 
+                       (gmgame.black.result === "50move" ? "Draw by 50-move rule" : 
+                       (gmgame.black.result === "kingofthehill" ? "Opponent king reached the hill" : 
+                       (gmgame.black.result === "threecheck" ? "Checked for the 3rd time" : 
+                       (gmgame.black.result === "timevsinsufficient" ? "Draw by timeout vs insufficient material" :
+                       (gmgame.black.result === "bughousepartnerlose" ? "Bughouse partner lost" : null)))))))))))))
+        const url5 = gmgame.white["@id"]
+        const response5 = await fetch(url5)
+        const data5 = await response5.json()
+        gmopponentname = data5.name ? data5.name : data5.username
+      } else if (gmgame.white["@id"] === `https://api.chess.com/pub/player/${randomgms[i]}`) {
+        gmgameresult = gmgame.white.result === "win" ? "Win" : 
+                       (gmgame.white.result === "chechmated" ? "Checkmated" :
+                       (gmgame.white.result === "agreed" ? "Draw agreed" : 
+                       (gmgame.white.result === "repetition" ? "Draw by repetition" : 
+                       (gmgame.white.result === "timeout" ? "Timeout" : 
+                       (gmgame.white.result === "resigned" ? "Resigned" : 
+                       (gmgame.white.result === "stalemate" ? "Stalemate" : 
+                       (gmgame.white.result === "lose" ? "Lose" : 
+                       (gmgame.white.result === "insufficient" ? "Insufficient material" : 
+                       (gmgame.white.result === "50move" ? "Draw by 50-move rule" : 
+                       (gmgame.white.result === "kingofthehill" ? "Opponent king reached the hill" : 
+                       (gmgame.white.result === "threecheck" ? "Checked for the 3rd time" : 
+                       (gmgame.white.result === "timevsinsufficient" ? "Draw by timeout vs insufficient material" :
+                       (gmgame.white.result === "bughousepartnerlose" ? "Bughouse partner lost" : null)))))))))))))
+        const url5 = gmgame.black["@id"]
+        const response5 = await fetch(url5)
+        const data5 = await response5.json()
+        gmopponentname = data5.name ? data5.name : data5.username
+      }
+
+      const gmgamepgn = gmgame.pgn
+
+      const gmgameinfo = {
+        gmname: gmname,
+        gmopponentname: gmopponentname,
+        gmgametype: gmgametype,
+        gmgameresult: gmgameresult,
+        gmgamepgn: gmgamepgn
+      }
+
+      console.log(gmgameinfo)
+      games[i] = gmgameinfo
+    }
+
+    setGames(games)
+    // setLoading(false)
+  }, []) 
+
+  useEffect(() => {
+    getGMGames()
+  }, [getGMGames]) */
 
   return (
     <div className="learnwatch" data-theme={theme}>
@@ -61,11 +179,39 @@ function Learnwatch() {
           <GridLoader color={theme === "lightmode" ? "var(--primary)" : "var(--secondary)"} loading={loading} size={50} 
           speedMultiplier={1} style={{position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)",
           userSelect: "none"}}/>
-        </div> : null }
+        </div> : null}
+        {/* <div>
+          <div className="score_table" style={{zIndex: 0}}>
+            <table className="score_table_columns">
+              <thead className="score_table_header">
+                <tr>
+                  <th>Player</th>
+                  <th>Opponent</th>
+                  <th>Game type</th>
+                  <th>Result</th>
+                  <th>Watch</th>
+                </tr>
+              </thead>
+              {games.map((val, key) => {
+              return (
+                <tbody key={key}>
+                  <tr>
+                    <td><b>{val.gmname}</b></td>
+                    <td>{val.gmopponentname}</td>
+                    <td>{val.gmgametype}</td>
+                    <td>{val.gmgameresult}</td>
+                    <button>Watch</button>
+                  </tr>
+                </tbody>
+              )
+              })}
+            </table>
+          </div>
+          <div className="table_padding"/>
+            </div> } */}
       <SwitchThemeButton onClick={switchTheme} style={{zIndex: "9"}}>
         {theme === "lightmode" ? (<IoIcons.IoIosSunny />) : (<IoIcons.IoIosMoon />)}
       </SwitchThemeButton>
-      {/* <button onClick={() => getGMs()}>button</button> */}
     </div>
   )
 }
